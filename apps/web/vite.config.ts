3884ef1759a2158a104e8b562fd6a2a7b932f93b
Arun Selva Kumar <arun@joinassembly.com>
2026-01-13 19:48:51 +0530

    init: Added tanstack start project

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "url";
+
+const config = defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  plugins: [
+    devtools(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
